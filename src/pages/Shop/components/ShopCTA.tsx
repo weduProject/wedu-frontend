563b1f02b97e4dffa64831668dc5039f0735c87f
yeseun2005 <yeseun2005@sookmyung.ac.kr>
2026-07-26 16:10:44 +0900

    Reapply "Merge pull request #18 from weduProject/feature/like"
    This reverts commit 5d39d5cbd58333bb15f8435d3ac7d17bdf2110bc.

--- src/pages/Shop/components/ShopCTA.tsx
+++ src/pages/Shop/components/ShopCTA.tsx
@@ -4,7 +4,7 @@ export default function ShopCTA() {
   const navigate = useNavigate();
 
   return (
-    <section className="flex flex-col items-center rounded-3xl bg-gradient-to-r from-[#C36978] to-[#DDA06B] px-12 py-12 text-center">
+    <section className="flex flex-col items-center rounded-3xl bg-linear-to-r from-[#C36978] to-[#DDA06B] px-12 py-12 text-center">
       <h2 className="mb-3 text-3xl font-bold text-white">
         원하는 게 없으신가요?
       </h2>
